a344763461f2e714c26eeae9d45ec1d583b0a707
Lwandoxelo27 <138695827+Lwandoxelo27@users.noreply.github.com>
2024-07-21 04:43:49 +0200

    updated file

--- basic_operations.py
+++ basic_operations.py
@@ -2,6 +2,8 @@ number1 = 10
 number2 = 5
 number1 == 10
 number2 == 5
+number1 - number2
+number1 + number2
 print("Addition of 10 and 5 is 15")
 print("Subtraction of 10 and 5 is 5")
 print("Multiplication of 10 and 5 is 50")
