d2f49fe31b2576cb3a3e804c94a1779b7b53f8ed
Lwandoxelo27 <138695827+Lwandoxelo27@users.noreply.github.com>
2024-08-18 19:34:58 +0200

    new edit

--- basic_operations.py
+++ basic_operations.py
@@ -3,11 +3,11 @@ number1 = 10
 number2 = 5
 
 # Perform the operations
-sum_result = number1 + number2
-difference_result = number1 - number2
-product_result = number1 * number2
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
 
 # Print the results
-print(f"Sum of {number1} and {number2} is {sum_result}")
-print(f"Difference of {number1} and {number2} is {difference_result}")
-print(f"Product of {number1} and {number2} is {product_result}")
+print(f"Sum of {number1} and {number2} is {sum}")
+print(f"Difference of {number1} and {number2} is {difference}")
+print(f"Product of {number1} and {number2} is {product}")
