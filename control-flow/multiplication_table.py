a8dc95feecf75412954258be72247428c40f8eaf
Lwandoxelo27 <138695827+Lwandoxelo27@users.noreply.github.com>
2024-08-19 13:40:40 +0200

    added code

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,7 @@
+# Prompt user for a Number
+number = int(input("Enter a number to see its multiplication table: "))
+
+# Generate and Print the Multiplication Table
+for i in rage(1, 10):
+    product = number * i
+    print(f"{number} * {i} = {product}")
