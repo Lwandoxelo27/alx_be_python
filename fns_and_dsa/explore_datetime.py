2f40d50cf2bbd2b2c3d798fca4c244249f2d1386
Lwandoxelo27 <138695827+Lwandoxelo27@users.noreply.github.com>
2024-08-19 16:04:45 +0200

    code fixes because of txt

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -7,7 +7,7 @@ def display_current_datetime():
 
 # Part 2: Calculate a Future Date
 def calculate_future_date():
-    days = int(input("Enter the number of days to add to current date: "))
+    days = int(input("Enter the number of days to add to the current date: "))
     current_date = datetime.now()
     future_date = current_date + timedelta(days=number_of_days)
     print("Future Date:", future_date.strftime("%Y-%m-%d"))
